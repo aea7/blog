2545637355f04f9df31275b34f97dba13904c4ba
Abdullah Ak <aeak1994@gmail.com>
2016-06-16 14:25:18 +0000

    login gg

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,24 @@ class SessionsController < ApplicationController
   
   
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
     
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.user_id
+      flash[:success] = "You have logged in"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "Invalid password or email"
+      render 'new'
+    end
+      
   end
   
   
   def destroy
-    
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   
   get 'login',to: 'sessions#new'
   post 'login',to: 'sessions#create'
-  delete 'logut',to: 'sessions#create'
+  delete 'logout',to: 'sessions#create'
   
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
