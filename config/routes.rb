7e4b47ee787169ce46854e1db0ceab99d5a8b42c
Abdullah Ak <aeak1994@gmail.com>
2016-06-16 13:31:07 +0000

    login page done

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,13 @@ Rails.application.routes.draw do
    root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  
   get 'signup', to: 'users#new'
   resources :users,except: [:new]
+  
+  get 'login',to: 'sessions#new'
+  post 'login',to: 'sessions#create'
+  delete 'logut',to: 'sessions#create'
   
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
