3668c922ca9556d0445640017aa81542427140cd
Abdullah Ak <aeak1994@gmail.com>
2016-06-17 07:50:37 +0000

    login logout done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,8 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       flash[:success] = "You have logged in"
       redirect_to user_path(user)
     else
