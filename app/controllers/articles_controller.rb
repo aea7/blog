d459d19f45825878522368a322992f30b7b17638
Abdullah Ak <aeak1994@gmail.com>
2016-06-21 07:15:37 +0000

    doni dan

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -48,7 +48,7 @@ class ArticlesController < ApplicationController
   private
   
     def require_same_user
-      if current_user != @article.user and !current.user.admin?
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "You can only edit your own article"
         redirect_to root_path 
       end 
